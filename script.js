3b56aa16226504d260b5c932c170e8d3344e9ddc
Ambar Martin <110828582+Lavendas@users.noreply.github.com>
2025-10-23 21:24:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ async function datos(raw) {
     }
 } 
 
-datos("https://lavendas.github.io/Clase10/datos.json");
+datos("https://raw.githubusercontent.com/Lavendas/Clase10/refs/heads/main/datos.json");
+
 
 
 
